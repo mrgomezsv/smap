14777c90699511dd8127888bf9178c2bad9c7540
Mario Roberto <mrgomez.dev@gmail.com>
2024-04-10 16:07:18 -0600

    Se edito el sql

--- ScriptsDB/create_users_table.sql
+++ ScriptsDB/create_users_table.sql
@@ -8,3 +8,43 @@ CREATE TABLE users (
 
 INSERT INTO users (username, password, fullname, role)
 VALUES ('mrgomez', 'Karin2100', 'Mario Roberto', 'dev');
+
+CREATE TABLE product (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    category INTEGER NOT NULL,
+    featured_image VARCHAR(255),
+    description TEXT,
+    additional_images TEXT[]
+);
+
+INSERT INTO product (title, category, featured_image, description, additional_images)
+VALUES ('Producto Ejemplo 1', 1, 'ruta/a/imagen/principal.jpg', 'Descripción del producto', ARRAY['ruta/a/imagen1.jpg', 'ruta/a/imagen2.jpg']);
+
+INSERT INTO product (title, category, featured_image, description, additional_images)
+VALUES ('Producto Ejemplo 2', 1, 'ruta/a/imagen/principal.jpg', 'Descripción del producto', ARRAY['ruta/a/imagen1.jpg', 'ruta/a/imagen2.jpg']);
+
+INSERT INTO product (title, category, featured_image, description, additional_images)
+VALUES ('Producto Ejemplo 3', 1, 'ruta/a/imagen/principal.jpg', 'Descripción del producto', ARRAY['ruta/a/imagen1.jpg', 'ruta/a/imagen2.jpg']);
+
+INSERT INTO product (title, category, featured_image, description, additional_images)
+VALUES ('Producto Ejemplo 4', 1, 'ruta/a/imagen/principal.jpg', 'Descripción del producto', ARRAY['ruta/a/imagen1.jpg', 'ruta/a/imagen2.jpg']);
+
+INSERT INTO product (title, category, featured_image, description, additional_images)
+VALUES ('Producto Ejemplo 5', 1, 'ruta/a/imagen/principal.jpg', 'Descripción del producto', ARRAY['ruta/a/imagen1.jpg', 'ruta/a/imagen2.jpg']);
+
+INSERT INTO product (title, category, featured_image, description, additional_images)
+VALUES ('Producto Ejemplo 6', 1, 'ruta/a/imagen/principal.jpg', 'Descripción del producto', ARRAY['ruta/a/imagen1.jpg', 'ruta/a/imagen2.jpg']);
+
+INSERT INTO product (title, category, featured_image, description, additional_images)
+VALUES ('Producto Ejemplo 7', 1, 'ruta/a/imagen/principal.jpg', 'Descripción del producto', ARRAY['ruta/a/imagen1.jpg', 'ruta/a/imagen2.jpg']);
+
+INSERT INTO product (title, category, featured_image, description, additional_images)
+VALUES ('Producto Ejemplo 8', 1, 'ruta/a/imagen/principal.jpg', 'Descripción del producto', ARRAY['ruta/a/imagen1.jpg', 'ruta/a/imagen2.jpg']);
+
+INSERT INTO product (title, category, featured_image, description, additional_images)
+VALUES ('Producto Ejemplo 9', 1, 'ruta/a/imagen/principal.jpg', 'Descripción del producto', ARRAY['ruta/a/imagen1.jpg', 'ruta/a/imagen2.jpg']);
+
+INSERT INTO product (title, category, featured_image, description, additional_images)
+VALUES ('Producto Ejemplo 10', 1, 'ruta/a/imagen/principal.jpg', 'Descripción del producto', ARRAY['ruta/a/imagen1.jpg', 'ruta/a/imagen2.jpg']);
+
